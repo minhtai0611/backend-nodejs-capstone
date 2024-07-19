865585ba4f92735a076b2a5c23650a93f68e0557
minhtai0611 <minhtainth06111999@gmail.com>
2024-07-19 06:10:43 -0400

    commit

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -11,7 +11,7 @@ const {loadData} = require("./util/import-mongo/index");
 
 const app = express();
 app.use("*",cors());
-const port = 3062;
+const port = 3060;
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
